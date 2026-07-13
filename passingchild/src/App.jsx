96f1c9054f02355bd2bf864e6d46ded5f75b49ee
sangeet <sangeet40404@gmail.com>
2026-07-13 11:12:56 +0530

    added

---
+++ passingchild/src/App.jsx
@@ -0,0 +1,28 @@
+import react from "react";
+import Container from "./component/container";
+
+let items = ["oil", "icecream", "charger", "books", "lights", "toy", "netflix"];
+
+function App(){
+
+
+  return <> 
+  
+  <Container></Container>
+  <center>
+        <h1>Shop list</h1>
+        
+
+        {items.length === 0 ? <h4>Shop is empty</h4> : null}
+        {items.length === 0 && <h4>Shop is empty</h4>}
+        
+        <ul className="list-group">
+          {items.map((item, index) => (
+            <li key={index} className="list-group-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </center></>
+}
+export default App;
---
+++ passingchild/src/component/container.jsx
@@ -0,0 +1,10 @@
+
+import React from "react"
+import styles from "./container.module.css";
+function Container(){
+return <>
+<div className="box">sd</div>
+
+</>
+}
+export default Container;
